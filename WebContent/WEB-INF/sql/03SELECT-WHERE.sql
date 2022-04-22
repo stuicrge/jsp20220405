0bf0cfe43b5ef6464fddf6f5ef6fa2ddd63696dd
김재훈 <wosl1006@naver.com>
2022-04-22 09:54:26 +0900

    JDBC2

---
+++ WebContent/WEB-INF/sql/03SELECT-WHERE.sql
@@ -0,0 +1,21 @@
+USE w3schools;
+
+SELECT * FROM Employees;
+SELECT FirstName FROM Employees;
+SELECT CustomerName FROM Customers;
+SELECT Address, CustomerName FROM Customers;
+
+-- WHERE : 어떤 ROW를 선택할 지 결정해주는 절(키워드)
+SELECT * FROM Customers
+WHERE Country = 'Mexico';
+SELECT *FROM Customers
+WHERE Country = 'Germany';
+
+-- 작은 따움표 : 문자열값
+-- ex3) 고객ID가 1번인 고객조회
+SELECT * FROM Customers WHERE CustomerID = 1; -- 수형식은 작은 따움표 안씀 써도 되긴함
+SELECT * FROM Customers WHERE CUstomerID = 90;
+SELECT * FROM Customers WHERE CustomerID = '1';
+SELECT * FROM Cusomers WHERE Country = Germany; -- 따움표르 안써서 에러발생
+
+SELECT * FROM CustomerName From Customers WHERE CustomerID = 1; 
